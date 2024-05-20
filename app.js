e70b7679759b5d53936c4efa9176e4b1961ffc6a
Zainab Murtaza <162459926+Zainab090@users.noreply.github.com>
2024-05-20 21:59:15 +0500

    Assignment#2 of Javascript

---
+++ app.js
@@ -0,0 +1,30 @@
+//Assignment#1
+let username;
+//Assignment#2
+let myName="Zainab MUrtaza";
+//Assignment#3
+let message="Hello World!";
+alert(message);
+//Assignment#4
+let userName="Jhone Doe";
+let age=15;
+let information="Certified Mobile Application Development";
+alert(userName);
+alert(age+ " years old." );
+alert(information);
+//Assignment#4
+let text="PIZZA\nPIZZ\nPIZ\nPI\np";
+alert(text);
+//Assignment#5
+let email=" example@example.com";
+alert("My email adress is "+ email);
+//Assignment#6
+let book="A smarter way to learn Javascript";
+alert("I'm trying to learn from the Book "+book);
+//Assignment#7
+document.write("Yah!I can write HTML content through JavaScript");
+//Assignment#8
+let design="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(design);
+document.write("<br><br>“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+//End
